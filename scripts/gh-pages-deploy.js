a8d3993b23f587ce5c97adfdba1c716861e1ebe3
Ondřej <ondra.stolz@seznam.cz>
2021-11-30 21:49:08 +0100

    Auto deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -13,6 +13,7 @@ import fs from "fs";
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    console.log("Remove folder");
     await execa("del", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
