6c819cd7d581ed7a6053d1ac06d314773048b67a
Ondřej <ondra.stolz@seznam.cz>
2022-12-09 02:01:57 +0100

    Removed # from url

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Stations from "../views/Stations";
 
@@ -41,7 +41,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
