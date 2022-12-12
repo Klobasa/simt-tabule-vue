6580fc86368cf11860eb42cfaf36cd572c6437b2
Ondřej <ondra.stolz@seznam.cz>
2022-12-12 01:01:31 +0100

    createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Stations from "../views/Stations";
 
@@ -41,7 +41,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/simt-tabule-vue/"),
+  history: createWebHashHistory(),
   routes,
 });
 
